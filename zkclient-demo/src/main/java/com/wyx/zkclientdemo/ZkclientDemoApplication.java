52297ac3b21e3ac5087b344873be175342751653
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-10-29 17:53:24 +0800

    add

---
+++ zkclient-demo/src/main/java/com/wyx/zkclientdemo/ZkclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.zkclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkclientDemoApplication.class, args);
+	}
+
+}
---
+++ zkclient-demo/src/test/java/com/wyx/zkclientdemo/ZkclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.zkclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
