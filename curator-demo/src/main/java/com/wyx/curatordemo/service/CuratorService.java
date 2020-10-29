b7d68a7465bd2aee1c6ca2a849798f6ac3783c8c
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-10-29 18:56:40 +0800

    add curator

---
+++ curator-demo/src/main/java/com/wyx/curatordemo/service/CuratorService.java
@@ -0,0 +1,12 @@
+package com.wyx.curatordemo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description : CuratorService
+ * @author : Just wyx
+ * @Date : 2020/10/29
+ */
+@Service
+public class CuratorService {
+}
---
+++ curator-demo/src/test/java/com/wyx/curatordemo/CuratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.curatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
